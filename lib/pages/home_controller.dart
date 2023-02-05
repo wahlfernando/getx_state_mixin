944e6206a73bc8499b1b4cd4c99a045094c62d6d
Fernando Alexandre Wahl <>
2023-02-05 11:47:14 -0300

    mudando para append

--- lib/pages/home_controller.dart
+++ lib/pages/home_controller.dart
@@ -3,7 +3,7 @@ import 'package:get_state_mixin/models/cep_model.dart';
 
 import 'package:get_state_mixin/repository/viacep_repository.dart';
 
-class HomeController extends GetxController with StateMixin<CepModel>{
+class HomeController extends GetxController with StateMixin<CepModel> {
   final ViacepRepository _repository;
 
   final _cepSearch = ''.obs;
@@ -14,17 +14,19 @@ class HomeController extends GetxController with StateMixin<CepModel>{
   HomeController(this._repository);
 
   @override
-  void onReady(){
+  void onReady() {
     change(state, status: RxStatus.empty());
   }
 
   Future<void> findAddress() async {
-    try {
-      change(state, status: RxStatus.loading());
-      final cep = await _repository.getCep(_cepSearch.value);
-      change(cep, status: RxStatus.success());
-    } catch (e) {
-      change(state, status: RxStatus.error());
-    }
+    change(state, status: RxStatus.loading());
+    append(() => () {
+      final cep = _cepSearch.value;
+      return _repository.getCep(cep);
+    });
   }
+
+  // O metodo "append" ja traz todos os beneficios do erro e sucesso incorporado para os estados que devemos usar na tela. 
+
+ 
 }
